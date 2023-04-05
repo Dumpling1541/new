ea921036c24842f4b44b8cb0186d73bfd26b00dd
mama <jungchunza@naver.com>
2023-04-05 09:42:35 +0900

    feat:기본페이지구현

--- src/main/java/controller/moim/MoimSearchController.java
+++ src/main/java/controller/moim/MoimSearchController.java
@@ -2,7 +2,9 @@ package controller.moim;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,9 +12,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
 import data.Moim;
 import repository.Moims;
-
 
 @WebServlet("/moim/search")
 public class MoimSearchController extends HttpServlet{
@@ -20,28 +24,31 @@ public class MoimSearchController extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		String[] cates = req.getParameterValues("cate"); //여러개의 데이터 넘길때
-//		System.out.println(Arrays.toString(cates));
-		Moims.findByCate(cates);
-	
+		String[] cates =req.getParameterValues("cate");
+		// System.out.println(Arrays.toString(cates));
+		
+		int p;
+		if(req.getParameter("page")==null) {
+			p =1;
+		}else {
+			p = Integer.parseInt(req.getParameter("page"));
+		}
 		
 		
-		List<Moim> list =Moims.findByCate(cates);
+		
+		SqlSessionFactory factory = (SqlSessionFactory) req.getServletContext().getAttribute("sqlSessionFactory");
+		SqlSession sqlSession = factory.openSession();
+		Map<String,Object> map = new HashMap<>();
+		
+		map.put("a", 1);
+		map.put("b", 6*p);
+		
+		List<Moim> list = sqlSession.selectList("moims.findSomeByAtoB", map);
+		//		List<Moim> list =Moims.findByCate(cates);
+		sqlSession.close();
+		
 		req.setAttribute("list", list);
-//		System.out.println(list);
-		
-//		
-//		String cate = req.getParameter("cate");
-//		
-//		if(cate != null) {
-//			List<Moim> li = Moims.findCate(cate);
-//			req.setAttribute("cate", li);			
-//			
-//		}else {
-//			List<Moim> li = Moims.findAll();
-//			req.setAttribute("cate", li);
-//		}
-//		
+				
 		req.getRequestDispatcher("/WEB-INF/views/moim/search.jsp").forward(req, resp);		
 	}
 }
